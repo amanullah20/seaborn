fc26b01a77f99251fe8528fe0bae6fb618536f5e
Aman Ullah <67518111+amanullah20@users.noreply.github.com>
2024-11-16 23:19:05 +0600

    Create 95_lineplot.py

---
+++ examples/95_lineplot.py
@@ -0,0 +1,6 @@
+#Example: 95
+
+import seaborn as sns; import matplotlib.pyplot as plt
+data = sns.load_dataset("flights")
+sns.lineplot(x="year", y="passengers", hue="month", data=data)
+plt.show()
