bf22fe80ba9a6e4ebd0dc10eb2e34ba64242ae2b
Aman Ullah <67518111+amanullah20@users.noreply.github.com>
2024-11-16 12:20:20 +0600

    Create 10_scatterplot.py

---
+++ examples/10_scatterplot.py
@@ -0,0 +1,6 @@
+#Example: 10
+
+import seaborn as sns; import matplotlib.pyplot as plt
+data = sns.load_dataset("tips")
+sns.scatterplot(x="total_bill", y="tip", size="size", hue="size", data=data, sizes=(20, 200))
+plt.show()
