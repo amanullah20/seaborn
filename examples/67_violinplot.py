934f3fcbef29949cfaaae7c0b6cdd0efdbcee7d7
Aman Ullah <67518111+amanullah20@users.noreply.github.com>
2024-11-16 22:00:47 +0600

    Create 67_violinplot.py

---
+++ examples/67_violinplot.py
@@ -0,0 +1,6 @@
+#Example: 67
+
+import seaborn as sns; import matplotlib.pyplot as plt
+data = sns.load_dataset("tips")
+sns.violinplot(x="day", y="total_bill", hue="sex", data=data, split=True)
+plt.show()
