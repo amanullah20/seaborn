7fe6a4b136e6ad7deae3bbbb2ce73d2a73b9f088
Aman Ullah <67518111+amanullah20@users.noreply.github.com>
2024-11-16 14:37:52 +0600

    Create 22_catplot.py

---
+++ examples/22_catplot.py
@@ -0,0 +1,6 @@
+#Example: 22
+
+import seaborn as sns; import matplotlib.pyplot as plt
+data = sns.load_dataset("tips")
+sns.catplot(x="day", y="total_bill", hue="sex", kind="violin", split=True, data=data)
+plt.show()
