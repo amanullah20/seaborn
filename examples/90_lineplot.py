0060e52331eefad3b123e97ae7f1905f07fd3644
Aman Ullah <67518111+amanullah20@users.noreply.github.com>
2024-11-16 23:14:28 +0600

    Create 90_lineplot.py

---
+++ examples/90_lineplot.py
@@ -0,0 +1,7 @@
+#Example: 90
+
+import seaborn as sns; import matplotlib.pyplot as plt
+data = sns.load_dataset("flights")
+sns.lineplot(x="year", y="passengers", data=data)
+plt.yscale("log")
+plt.show()
