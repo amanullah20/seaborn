da6272da8bfb92d2d28a6ed1fb8f4aca983c6599
Aman Ullah <67518111+amanullah20@users.noreply.github.com>
2024-11-16 14:28:41 +0600

    Create 16_heatmap.py

---
+++ examples/16_heatmap.py
@@ -0,0 +1,6 @@
+#Example: 16
+
+import seaborn as sns; import matplotlib.pyplot as plt
+data = sns.load_dataset("tips")
+sns.heatmap(data.pivot_table(index="day", columns="time", values="total_bill", aggfunc="mean"), annot=True, cmap="YlGnBu")
+plt.show()
