30c3a2e3cc752e1fd1c21b94db9a19251a9ca0a6
Aman Ullah <67518111+amanullah20@users.noreply.github.com>
2024-11-16 15:04:53 +0600

    Create 36_heatmap.py

---
+++ examples/36_heatmap.py
@@ -0,0 +1,6 @@
+#Example: 36
+
+import seaborn as sns; import matplotlib.pyplot as plt
+data = sns.load_dataset("tips")
+sns.heatmap(data.corr(), cmap=sns.diverging_palette(220, 20, as_cmap=True), annot=True)
+plt.show()
