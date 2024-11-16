21c0b306f52647bf8b6c08d4d5d2d643e09fd465
Aman Ullah <67518111+amanullah20@users.noreply.github.com>
2024-11-16 23:16:58 +0600

    Create 93_catplot.py

---
+++ examples/93_catplot.py
@@ -0,0 +1,6 @@
+#Example: 93
+
+import seaborn as sns; import matplotlib.pyplot as plt
+data = sns.load_dataset("tips")
+sns.catplot(x="day", y="total_bill", hue="sex", kind="bar", data=data)
+plt.show()
