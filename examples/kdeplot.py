c27854b411207669b04dd52f04f33a64f7a9694d
Aman Ullah <67518111+amanullah20@users.noreply.github.com>
2024-11-16 23:10:14 +0600

    Create kdeplot.py

---
+++ examples/kdeplot.py
@@ -0,0 +1,6 @@
+#Example: 87
+
+import seaborn as sns; import matplotlib.pyplot as plt
+data = sns.load_dataset("tips")
+sns.kdeplot(data=data, x="total_bill", hue="sex", multiple="layer", fill=True)
+plt.show()
