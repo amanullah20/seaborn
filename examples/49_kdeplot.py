841d603de1a3125b33187b5b0defdab8ab271d9a
Aman Ullah <67518111+amanullah20@users.noreply.github.com>
2024-11-16 21:38:02 +0600

    Create 49_kdeplot.py

---
+++ examples/49_kdeplot.py
@@ -0,0 +1,7 @@
+#Example: 49
+
+import seaborn as sns; import matplotlib.pyplot as plt
+data = sns.load_dataset("tips")
+g = sns.FacetGrid(data, col="sex")
+g.map(sns.kdeplot, "total_bill")
+plt.show()
