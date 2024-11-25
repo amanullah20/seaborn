f25be5681de0a12a5dd377b568049a8d2876cb98
Aman Ullah <67518111+amanullah20@users.noreply.github.com>
2024-11-25 23:43:03 +0600

    Create 133_kdeplot.py

---
+++ examples/133_kdeplot.py
@@ -0,0 +1,6 @@
+#Example: 133
+
+import seaborn as sns; import matplotlib.pyplot as plt
+data = sns.load_dataset("tips")
+sns.kdeplot(data=data, x="total_bill", hue="sex", multiple="fill")
+plt.show()
