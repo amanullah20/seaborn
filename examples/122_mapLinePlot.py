4e62e477bef14e40177045bc2cb7e20fcd399455
Aman Ullah <67518111+amanullah20@users.noreply.github.com>
2024-11-25 23:18:26 +0600

    Create 122_mapLinePlot.py

---
+++ examples/122_mapLinePlot.py
@@ -0,0 +1,7 @@
+#Example: 122
+
+import seaborn as sns; import matplotlib.pyplot as plt
+data = sns.load_dataset("flights")
+g = sns.FacetGrid(data, col="month", col_wrap=4, height=2.5)
+g.map(sns.lineplot, "year", "passengers")
+plt.show()
